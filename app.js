00a7f45992b7de00ef4e3d3413724a93292322e6
arlankarlos <lolozo@msn.com>
2024-09-06 19:52:26 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+/**
+ * Preenche a div com id "resultado-pesquisa" com uma lista
+ * de destaque com titulo e descricao de cada objeto na lista
+ * "dados".
+ */
+function pesquisar() {
+  let section = document.getElementById("resultado-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  if (campoPesquisa ==""){
+    section.innerHTML = "<div class='destaque'>Insira um termo para pesquisar</div>";
+    return
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+  let lowerCaseTitulo = ""
+  let lowerCaseDescricao = ""
+  let lowerCaseTags = ""
+
+  let resultados = "";
+
+  // exibe cada objeto dentro da lista de planetas
+  for (let dado of dados) {
+    lowerCaseTitulo = dado.titulo.toLowerCase()
+    lowerCaseDescricao = dado.descricao.toLowerCase()
+    lowerCaseTags = dado.tags.toLowerCase()
+
+    if (lowerCaseTitulo.includes(campoPesquisa) || 
+    lowerCaseDescricao.includes(campoPesquisa) ||
+    lowerCaseTags.includes(campoPesquisa)) {
+      resultados += `
+         <div class="destaque">
+            <h2>${dado.titulo}</h2>
+             <p>
+                ${dado.descricao}
+               </p>
+            </div>
+            <br />
+        `;
+    }
+  }
+
+  if (resultados == "") {
+    resultados = '<div class="destaque">Nenhum resultado encontrado</div>';
+  }
+
+  section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,92 @@
+let dados = [
+    {
+        titulo: "Desvendando os Mistérios do Sistema Solar",
+        descricao: "Já se perguntou como se formou o nosso sistema solar? Quais são os planetas que o compõem e quais as suas características únicas? Neste artigo, faremos uma viagem fascinante pelo nosso quintal cósmico, explorando desde o incandescente Sol até os gelados confins do Sistema Solar. O Sistema Solar é um conjunto de planetas, asteroides e cometas que giram ao redor do Sol. Ele é composto por oito planetas, que são Mercúrio, Vênus, Terra, Marte, Júpiter, Saturno, Urano e Netuno. Além disso, o Sistema Solar possui planetas anões, como Plutão, e corpos celestes, como asteroides e cometas.",
+        tags: "sistema solar planetas sol exploração espacial"
+    },
+    {
+        titulo: "O Sol: A Estrela do Nosso Sistema",
+        descricao: "No centro de tudo está o Sol, um imensa esfera de gás incandescente que fornece luz e calor para todos os planetas. Descubra como a energia do Sol é gerada e como ela influencia a vida na Terra.",
+        tags: "sol estrela energia vida"
+    },
+    {
+        titulo: "Os Planetas Rochosos",
+        descricao: "Mercúrio, Vênus, Terra e Marte são os planetas mais próximos do Sol e possuem superfícies sólidas. Explore as características únicas de cada um deles, desde o pequeno Mercúrio até o misterioso Marte.",
+        tags: "planetas rochosos Mercúrio Vênus Terra Marte",
+    },
+    {
+        titulo: "Planetas Anões e Corpos Celestes",
+        descricao: "Júpiter, Saturno, Urano e Netuno são gigantes gasosos, compostos principalmente por hidrogênio e hélio. Descubra as grandes tempestades de Júpiter, os anéis de Saturno e os ventos gelados de Urano e Netuno.",
+        tags: "planetas anões Plutão asteroides cometas",
+    },
+    {
+        titulo: "Os Gigantes Gasosos",
+        descricao: " Júpiter, Saturno, Urano e Netuno são gigantes gasosos, compostos principalmente por hidrogênio e hélio. Descubra as grandes tempestades de Júpiter, os anéis de Saturno e os ventos gelados de Urano e Netuno.",
+        tags: "gigantes gasosos Júpiter Saturno Urano Netuno",
+    },
+    {
+        titulo: "A Exploração Espacial",
+        descricao: "Ao longo da história, a humanidade sempre olhou para o céu com curiosidade. Descubra as principais missões espaciais que revolucionaram nosso conhecimento sobre o Sistema Solar e os desafios da exploração espacial.",
+        tags: "exploração espacial missões espaciais astronomia cosmos",
+    },
+    ];
+
+let planetas = [
+{
+nome: "Mercúrio",
+massa: 0.055, // Em relação à massa da Terra
+distanciaTerra: 57.9, // Em milhões de km
+distanciaSol: 57.9, // Em milhões de km
+descricao: "O menor e mais próximo do Sol dos oito planetas clássicos do Sistema Solar."
+},
+{
+nome: "Vênus",
+massa: 0.815,
+distanciaTerra: 41.4,
+distanciaSol: 108.2,
+descricao: "O segundo planeta mais próximo do Sol e o mais quente do sistema solar."
+},
+{
+nome: "Terra",
+massa: 1,
+distanciaTerra: 0,
+distanciaSol: 149.6,
+descricao: "O terceiro planeta a partir do Sol e o único com vida conhecida."
+},
+{
+nome: "Marte",
+massa: 0.107,
+distanciaTerra: 78.3,
+distanciaSol: 227.9,
+descricao: "Conhecido como o 'Planeta Vermelho', Marte é um dos mais estudados do sistema solar."
+},
+{
+nome: "Júpiter",
+massa: 317.8,
+distanciaTerra: 628.7,
+distanciaSol: 778.5,
+descricao: "O maior planeta do sistema solar, um gigante gasoso com uma Grande Mancha Vermelha."
+},
+{
+nome: "Saturno",
+massa: 95.2,
+distanciaTerra: 1275,
+distanciaSol: 1427,
+descricao: "Famoso por seus anéis, Saturno é outro gigante gasoso."
+},
+{
+nome: "Urano",
+massa: 14.5,
+distanciaTerra: 2872.5,
+distanciaSol: 2875,
+descricao: "Um gigante de gelo, Urano rotaciona de lado em relação ao plano de sua órbita."
+},
+{
+nome: "Netuno",
+massa: 17.1,
+distanciaTerra: 4504,
+distanciaSol: 4504,
+descricao: "O planeta mais distante do Sol e um gigante de gelo."
+}
+];
+
